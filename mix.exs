190cc5d627f46b64a22d258e9c5179a22eb00299
JufeBrown <jufebrown@gmail.com>
2025-06-04 14:12:29 -0500

    added httpoison to dependencies

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule GmeApp.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:httpoison, "~> 2.0"}
     ]
   end
 
