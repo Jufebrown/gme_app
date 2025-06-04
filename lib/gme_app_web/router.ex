42c76c21d10d9708d852a200e934c4c702cef6b8
JufeBrown <jufebrown@gmail.com>
2025-06-04 13:51:55 -0500

    edited route for api

--- lib/gme_app_web/router.ex
+++ lib/gme_app_web/router.ex
@@ -7,6 +7,8 @@ defmodule GmeAppWeb.Router do
 
   scope "/api", GmeAppWeb do
     pipe_through :api
+
+    get "/quotes/gme", QuoteController, :index
   end
 
   # Enable LiveDashboard and Swoosh mailbox preview in development
