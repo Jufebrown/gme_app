b02adf37cb169c4ead23b5738a8a7d5826ac77be
JufeBrown <jufebrown@gmail.com>
2025-06-08 19:35:30 -0500

    refactored controller to just send data from db

--- lib/gme_app_web/controllers/api_controller.ex
+++ lib/gme_app_web/controllers/api_controller.ex
@@ -1,23 +1,9 @@
 defmodule GmeAppWeb.ApiController do
   use GmeAppWeb, :controller
   alias GmeApp.Market
-  alias GmeApp.Market.AlphaVantage
 
   def gme(conn, _params) do
-    IO.puts("Fetching from DB...")
-    data = Market.get_recent_quotes()
-
-    quotes =
-      if data == [] do
-        IO.puts("No data in DB. Fetching from Alpha Vantage...")
-        fetched = AlphaVantage.fetch_daily_quotes()
-        IO.inspect(fetched, label: "Fetched quotes")
-        Market.insert_quotes(fetched)
-        fetched
-      else
-        IO.inspect(data, label: "DB data")
-        data
-      end
+    quotes = Market.get_recent_quotes()
 
     result =
       Enum.map(quotes, fn %{date: d, close: c} ->
@@ -26,8 +12,6 @@ defmodule GmeAppWeb.ApiController do
           value: c
         }
       end)
-
-    IO.inspect(result, label: "Final result")
 
     json(conn, result)
   end
