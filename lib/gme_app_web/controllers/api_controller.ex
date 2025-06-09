fcc51420d70500daa57befdd7fffcda0ce1f7215
JufeBrown <jufebrown@gmail.com>
2025-06-08 19:16:37 -0500

    added logging to controller

--- lib/gme_app_web/controllers/api_controller.ex
+++ lib/gme_app_web/controllers/api_controller.ex
@@ -4,22 +4,31 @@ defmodule GmeAppWeb.ApiController do
   alias GmeApp.Market.AlphaVantage
 
   def gme(conn, _params) do
+    IO.puts("Fetching from DB...")
     data = Market.get_recent_quotes()
 
     quotes =
       if data == [] do
+        IO.puts("No data in DB. Fetching from Alpha Vantage...")
         fetched = AlphaVantage.fetch_daily_quotes()
+        IO.inspect(fetched, label: "Fetched quotes")
         Market.insert_quotes(fetched)
         fetched
       else
+        IO.inspect(data, label: "DB data")
         data
       end
 
-    json(conn, Enum.map(quotes, fn %{date: d, close: c} ->
-      %{
-        time: Date.to_iso8601(d),
-        value: c
-      }
-    end))
+    result =
+      Enum.map(quotes, fn %{date: d, close: c} ->
+        %{
+          time: Date.to_iso8601(d),
+          value: c
+        }
+      end)
+
+    IO.inspect(result, label: "Final result")
+
+    json(conn, result)
   end
 end
